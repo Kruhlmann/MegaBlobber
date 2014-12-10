f1a008ed69525e6032e6e48d775438125e1fcd59
Sebastián <scanevaro@hotmail.com>
2014-12-09 21:36:37 -0300

    google play services

---
+++ core/src/com/deeep/jam/Interfaces/ActionResolver.java
@@ -0,0 +1,18 @@
+package com.deeep.jam.Interfaces;
+
+/**
+ * Created by scanevaro on 09/12/2014.
+ */
+public interface ActionResolver {
+    public boolean getSignedInGPGS();
+
+    public void loginGPGS();
+
+    public void submitScoreGPGS(int score);
+
+    public void unlockAchievementGPGS(String achievementId);
+
+    public void getLeaderboardGPGS();
+
+    public void getAchievementsGPGS();
+}
